ee451ce6dbf6455ac5f4ffdfda59d79dbd1b92f4
ruben-balmis <71265617+ruben-balmis@users.noreply.github.com>
2023-05-04 22:19:13 +0200

    Add files via upload

---
+++ Ejercicios/Ejercicio2.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    //Ejercicio 2
+
+    public delegate void Infinitivo();
+    public static void Ser() => Console.WriteLine("To be");
+    public static void Correr() => Console.WriteLine("Run");
+    public static void Ver() => Console.WriteLine("See");
+    public static void Pensar() => Console.WriteLine("Think");
+    public static void Comer() => Console.WriteLine("Eat");
+
+    private static void Main(string[] args)
+    {
+        Infinitivo infinitivo = Ser;
+        infinitivo += Correr;
+        infinitivo += Ver;
+        infinitivo();
+        Console.WriteLine("");
+        infinitivo -= Ser;
+        infinitivo -= Ver;
+        infinitivo += Pensar;
+        infinitivo += Comer;
+        infinitivo();
+    }   
+}
---
+++ Ejercicios/Ejercicio3.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    //Ejercicio 3
+
+    public static void Mostrar<T>(T[,] matriz)
+    {
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    float[,] matrizFloat = new float[,]
+    { 
+        {3, 4, 5},
+        {2.4F, 4.4F, 5}
+    };
+    dynamic[,] matrizDynamic = new dynamic[,]
+    {
+        {"SAL", "AGUA", "AZUCAR" , "VINO"},
+        {"COLA", "CAFE", "ZUMO", "LECHE"}
+    };
+
+    Action<float[,]> accion = Mostrar;
+    accion(matrizFloat);
+    Action<dynamic[,]> accion2 = Mostrar;
+    accion2(matrizDynamic);
+}
